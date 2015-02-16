609939e2c8262b41ea3f8daca21899644c3263fb
evpoole <evpoole@gmail.com>
2015-02-15 23:28:17 -0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = float(raw_input())
+b = float(raw_input())
+c = a+b
+d= a-b
+e= a*b
+f = a/b
+g = a//b
+print("%.2f" % c)
+print("%.2f" % d)
+print("%.2f" % e)
+print("%.2f" % f)
+print("%.2f" % g)
